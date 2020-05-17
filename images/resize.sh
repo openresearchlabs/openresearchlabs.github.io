4b7c4cce57d5d9ef2667450cdc6f4ccd4c65dcef
Leo <leo.lahti@iki.fi>
2020-05-17 14:17:05 +0300

    site deployment

--- images/resize.sh
+++
@@ -1,5 +0,0 @@
-sudo apt-get install imagemagick 
-mkdir resized
-find . -name "*.jpg" | xargs -i convert -scale 40% {} ./resized/{} 
-find . -name "*.JPG" | xargs -i convert -scale 40% {} ./resized/{} 
-ls resized 
